fbd2929289009d5d09a7e9183d11675488fc5fab
Danila Kurgan <54518408+dkurgan@users.noreply.github.com>
2019-09-12 23:12:38 -0700

    upd

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -3,7 +3,11 @@ $who = fopen("/var/run/utmpx", 'r');
 date_default_timezone_set('America/Los_Angeles');
 while ($line = fread($who, 628)){
   $arr = unpack("A256user/A4id/A32line/IPid/Itype/Itime", $line);
-  if ($arr["type"] == 7)
-      echo $arr["user"]."     ".$arr["line"]."  ".date("M j H:i", $arr["time"])."\n";
+  if ($arr["type"] == 7){
+      echo trim($arr['user']) . " ";
+       echo trim($arr['line']) . " ";
+        $time = date("M j H:i", $arr["time"]);
+          echo $time . "\n";
+  }
     }
  ?>
