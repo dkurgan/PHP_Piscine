7168542b961464c4988a4f7e23cfc1a4bfb318b2
Danila Kurgan <54518408+dkurgan@users.noreply.github.com>
2019-09-12 23:15:50 -0700

    upd spaces

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -4,8 +4,8 @@ date_default_timezone_set('America/Los_Angeles');
 while ($line = fread($who, 628)){
   $arr = unpack("A256user/A4id/A32line/IPid/Itype/Itime", $line);
   if ($arr["type"] == 7){
-      echo trim($arr['user']) . " ";
-       echo trim($arr['line']) . " ";
+      echo trim($arr['user']) . "   ";
+       echo trim($arr['line']) . "   ";
         $time = date("M j H:i", $arr["time"]);
           echo $time . "\n";
   }
